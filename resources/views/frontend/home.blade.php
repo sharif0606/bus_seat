c1f3ccbe61c03c834664ad5125ea7beeb3e75137
sharif0606 <shariful0606@gmail.com>
2023-11-23 13:22:36 +0600

    home update

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,102 +1,98 @@
 @extends('frontend.app')
 @section('content')
-    <section class="hero-section">
-        <div class="container-fluid hero-image">
-        <div class="hero-text text-center">
-            <p class="mb-0">Where would you like to Go?</p>
-                <h1 class="mt-o"> Family-Friendly Bus <span>Tours</span> <br /> Fun for All Ages</h1>
-                        <a class="button-design" type="button" href="#">Booking Now</a>
-            </div>
-        </div>
-    </section>
-    <section class="bg-white">
-        <div class="div-design">
-            <div class="py-2 div-design-text">
-                {{-- <label for="" class="div-design-text-date">Date : </label>
-                <input type="date" name="" id="" class="me-5" />
-                <label for="">From: </label>
-                <input type="text" placeholder="select your Place"name=""id=""class="me-5"/>
-                <label for="">To: </label>
-                <input type="text" name="" id=""placeholder="select destination" class="me-5" />
-                <input type="search" class="bg-dark text-light   py-1 ps-4" name="" id="" placeholder="  Search"/>
-                <span class="bg-dark"> <a href=""><i class="fas fa-search text-white"></i></a> </span> --}}
-            </div>
-        </div>
-    </section>
-    <section>
-        <div class="container-fluid mt-2">
-            <div class="row ">
-                <div class="col-12 col-md-6 mb-2">
-                <div class="booking-form ">
-                    <div id="booking-going-on">
-                        <h4 class="text-dark  fw-bold">Bus Tickets Booking for Tour!</h4>
-                        <p >  Get tickets today before it's too late!</p>
-                        <div class="input-group">
-                            <label for="">Name</label>
-                            <input id="name-input" class="inp-style type="text" name="" placeholder="Enter your name"/>
-                        </div>
-                        <div class="input-group">
-                            <label for="">Contact No.</label>
-                            <input id="contact-no-input" class="inp-style" type="tel" name="" placeholder="+880-1819000000" />
-                        </div>
-                        <div class="input-group">
-                            <label for="">Tour Date</label>
-                            <input id="tour-date-input" class="inp-style" type="date" name="" required="required"  />
-                        </div>
-                        <div class="input-group">
-                            <div class="input-group">
-                                <label for="">Booking Date</label>
-                                <input id="booking-date-input" class="inp-style" type="date" name="" required="required"/> 
-                            </div>
-                                <label for="">Number of Seat Booking</label >
-                                <input id="counter" class="first-class-seatBooking-seat inp-style inp-width " readonly type="number" name="" value="0" /> 
-                            
-                                <label for="">Price Per Ticket</label>
-                                <input id="ticketPrice" class="inp-style inp-width " readonly  type="number"  name="" value="500"  />     
-                            
-                        </div> 
-                        <div class="input-group d-grid">
-                            <button class="booking-button-design py-2 my-1">  <a href="#"  class="text-decoration-none text-white">Booking Seat</a></button>       
-                        </div>
-                        <div class="calculations">
-                            
-                            <div class="amount ">
-                                    <div class="left" readonly>
-                                        <h4>Total</h4>
-                                    </div>
-                                    <div class="right" id="totalAmount">
-                                        <p id="total">$0</p>
-                                    </div>
-                            </div>
-                        </div>
-                        <button id="book-now" class="btn-style  py-2 my-2">Get Tickets!</button> 
-                    </div>
-                    <div style="padding: 2em 0; margin:2em 0 1em 0;" id="booking-successful">
-                        <!-- <span style="text-align: center;""><img src="images\ticket2.jpg" alt="" width="300"/></span> -->
-                        <h3 style="text-align: center;">Thank you so much!</h3>
-                        <p class="fw=200" style="text-align: center; color: #e33741;">You have successfully completed your booking.</p>
-                            <hr>
-                        
-                        <h5 class="text-center mt-3 booking-date-class">Name: 
-                            <span id="name-final-view"> invalid</span> , Contact No.:
-                            <span id="contact-no-final-view"> invalid </span>  
-                        </h5>
+  <section class="hero-section">
+    <div class="container-fluid hero-image">
+      <div class="hero-text text-center">
+        <p class="mb-0">Where would you like to Go?</p>
+        <h1 class="mt-o"> Family-Friendly Bus <span>Tours</span> <br /> Fun for All Ages</h1>
+        <a class="button-design" type="button" href="#">Booking Now</a>
+      </div>
+    </div>
+  </section>
+  <section class="bg-white">
+    <div class="div-design">
+      <div class="py-2 div-design-text">
+        {{-- <label for="" class="div-design-text-date">Date : </label>
+        <input type="date" name="" id="" class="me-5" />
+        <label for="">From: </label>
+        <input type="text" placeholder="select your Place"name=""id=""class="me-5"/>
+        <label for="">To: </label>
+        <input type="text" name="" id=""placeholder="select destination" class="me-5" />
+        <input type="search" class="bg-dark text-light   py-1 ps-4" name="" id="" placeholder="  Search"/>
+        <span class="bg-dark"> <a href=""><i class="fas fa-search text-white"></i></a> </span> --}}
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="container-fluid mt-2">
+      <div class="row ">
+        <div class="col-12 col-md-6 mb-2">
+          <div class="booking-form ">
+            <div id="booking-going-on">
+              <h4 class="text-dark  fw-bold">Bus Tickets Booking for Tour!</h4>
+              <p>  Get tickets today before it's too late!</p>
+              <form action="{{route('frontbooking')}}" method="post">
+                @csrf
+              
+                <div class="input-group">
+                  <label for="">Name</label>
+                  <input id="name-input" class="inp-style" type="text" name="name" placeholder="Enter your name"/>
+                </div>
+                <div class="input-group">
+                  <label for="">Contact No.</label>
+                  <input id="contact-no-input" class="inp-style" type="tel" name="contact_no" placeholder="+880-1819000000" />
+                </div>
+                <div class="input-group">
+                  <label for="">Tour Date</label>
+                  <input id="tour-date-input" class="inp-style" type="date" name="tour_date" required="required"  />
+                </div>
+                <div class="input-group">
+                  <label for="">Number of Seat Booking</label >
+                  <input id="counter" class="first-class-seatBooking-seat inp-style inp-width " readonly type="number" name="no_of_seat" value="0" /> 
+                  <input id="seat_list" readonly type="text" name="seat_list" value="" /> 
 
-                            <p class="booking-date-class mt-3">Your booking date: <span id="booking-date-final-view"></span> </p>
-                            <p class="concert-date-class">Your tour date: <span id="tour-date-final-view"></span> </p>
-                        <hr>
-                            <p>You have booked <span id="first-class-seatBooking-final-view"></span></p>
-                        
-                        <hr>
-                        <p>Your total cost is  <span id="total-final-view"></span> </p>
-                        <!-- <p><a href="abcdefgh@gmail.com">Send email</a></p> -->
+                  <label for="">Price Per Ticket</label>
+                  <input id="ticketPrice" class="inp-style inp-width " readonly  type="number"  name="price" value="500"  />
+                </div>
+                <div class="calculations">
+                  <div class="amount ">
+                    <div class="left" readonly>
+                      <h4>Total</h4>
                     </div>
+                    <div class="right" id="totalAmount">
+                      <p id="total">$0</p>
+                    </div>
+                  </div>
+                </div>
+                <button id="book-now" class="btn-style  py-2 my-2" type="submit">Get Tickets!</button> 
+              </form>
+            </div>
+              <div style="padding: 2em 0; margin:2em 0 1em 0;" id="booking-successful">
+                <!-- <span style="text-align: center;""><img src="images\ticket2.jpg" alt="" width="300"/></span> -->
+                <h3 style="text-align: center;">Thank you so much!</h3>
+                <p class="fw=200" style="text-align: center; color: #e33741;">You have successfully completed your booking.</p>
+                    <hr>
+                
+                <h5 class="text-center mt-3 booking-date-class">Name: 
+                  <span id="name-final-view"> invalid</span> , Contact No.:
+                  <span id="contact-no-final-view"> invalid </span>  
+                </h5>
+
+                    <p class="booking-date-class mt-3">Your booking date: <span id="booking-date-final-view"></span> </p>
+                    <p class="concert-date-class">Your tour date: <span id="tour-date-final-view"></span> </p>
+                <hr>
+                    <p>You have booked <span id="first-class-seatBooking-final-view"></span></p>
+                
+                <hr>
+                <p>Your total cost is  <span id="total-final-view"></span> </p>
+                <!-- <p><a href="abcdefgh@gmail.com">Send email</a></p> -->
+            </div>
                 </div> 
                 </div>    
                 <div class=" col-12 col-md-6  mt-3 desktop-seat">
                     
                     <h4 class="text-center text-dark  fw-bold ">Tourist Mini Bus Seat Layout</h4>
-                        <table class="w-100 seat_plan">
+                      <table class="w-100 seat_plan">
                             <tbody cl="seat-list">
                                 <tr>
                                     <th scope="col" class="border text-center ">
@@ -234,7 +230,7 @@
                                 </tr>
                                 
                             </tbody>
-                    </table>
+                      </table>
                 </div>
             </div>
         </div>
@@ -421,6 +417,7 @@
       }
       document.getElementById('counter').value = bookedSeats;
       document.getElementById('totalAmount').textContent = `Total Amount: ${totalAmount}`;
+      document.getElementById('seat_list').value=seatbooked.join();
   }
 
   // Add click event listeners to all buttons
